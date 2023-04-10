cb8aedad69ca6277b1862cfe98cb33f2bc75f86d
Mirko Lenz <mirko@mirkolenz.com>
2023-04-10 15:53:44 +0200

    fix: update nlp configs

--- arguelauncher/config/nlp.py
+++ arguelauncher/config/nlp.py
@@ -10,8 +10,8 @@ log = logging.getLogger(__name__)
 
 class NlpConfig(Enum):
     DEFAULT = auto()
-    TRF = auto()
     SBERT = auto()
+    USE = auto()
 
 
 NLP_CONFIG: dict[NlpConfig, nlp_pb2.NlpConfig] = {
@@ -19,16 +19,22 @@ NLP_CONFIG: dict[NlpConfig, nlp_pb2.NlpConfig] = {
         language="en",
         spacy_model="en_core_web_lg",
     ),
-    NlpConfig.TRF: nlp_pb2.NlpConfig(
-        language="en",
-        spacy_model="en_core_web_trf",
-    ),
     NlpConfig.SBERT: nlp_pb2.NlpConfig(
         language="en",
         embedding_models=[
             nlp_pb2.EmbeddingModel(
                 model_type=nlp_pb2.EmbeddingType.EMBEDDING_TYPE_SENTENCE_TRANSFORMERS,
-                model_name="stsb-mpnet-base-v2",
+                model_name="multi-qa-MiniLM-L6-cos-v1",
+                pooling_type=nlp_pb2.Pooling.POOLING_MEAN,
+            )
+        ],
+    ),
+    NlpConfig.USE: nlp_pb2.NlpConfig(
+        language="en",
+        embedding_models=[
+            nlp_pb2.EmbeddingModel(
+                model_type=nlp_pb2.EmbeddingType.EMBEDDING_TYPE_TENSORFLOW_HUB,
+                model_name="https://tfhub.dev/google/universal-sentence-encoder/4",
                 pooling_type=nlp_pb2.Pooling.POOLING_MEAN,
             )
         ],
