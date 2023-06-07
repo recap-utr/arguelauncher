34cbf4a39a378e87836982fa2a100d8466ebf13f
Mirko Lenz <mirko@mirkolenz.com>
2023-06-07 11:09:34 +0200

    build: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,33 +1,37 @@
-# https://devenv.sh/reference/options/
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ devenv.flakeModule ];
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, lib, ... }: {
-        devenv.shells.default = {
-          env = {
-            CPATH = "${pkgs.pcre.dev}/include";
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {
+        pkgs,
+        lib,
+        ...
+      }: {
+        devShells.default = let
+          python = pkgs.python311;
+          poetry = pkgs.poetry;
+        in
+          pkgs.mkShell.override {stdenv = pkgs.gccStdenv;} {
+            packages = [poetry python];
+            POETRY_VIRTUALENVS_IN_PROJECT = true;
+            LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc];
+            # CPATH = "${pkgs.pcre.dev}/include";
+            buildInputs = with pkgs; [pcre zlib];
+            shellHook = ''
+              ${lib.getExe poetry} env use ${lib.getExe python}
+              ${lib.getExe poetry} install --all-extras --no-root
+            '';
           };
-          packages = with pkgs; [ pcre ];
-          languages.python = {
-            enable = true;
-            package = pkgs.python311;
-            poetry = {
-              enable = true;
-              activate.enable = true;
-              install.enable = true;
-            };
-          };
-        };
       };
     };
 }
